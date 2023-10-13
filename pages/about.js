ca9df3777e492a875f229948b40a696466dcbaaf
lumijoe <spacedesign63@gmail.com>
2023-10-13 17:20:42 +0900

    vercel bug

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 // about.js(アバウトページ)
 
 import Hero from '@/components/Hero'
+import Link from 'next/link';
 
 export default function About() {
   return (
@@ -10,17 +11,9 @@ export default function About() {
          backgroundColor: 'lightblue',
          color: 'white'
       }}>This is ABOUT page.</p>
-      <a href='/blog'
-               style={{
-                    backgroundColor: 'black',
-                    color: 'white',
-                    padding: '3px 18px',
-                    boxSizing: 'border-box',
-                    border: '1px black solid',
-                    textDecoration: 'none',
-                    borderRadius: '9999px'
-               }}>BLOGへ</a>
+      <Link href='/blog'>
+        BLOGへ
+      </Link>
     </div>
-    
   )
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,7 @@
 // pages/blog/index.js(ブログページをblogディレクトリで管理する)
 
 import Hero from '@/components/Hero'
+import Link from 'next/link';
 
 export default function Blog() {
     return (
@@ -10,16 +11,9 @@ export default function Blog() {
                 backgroundColor: '#9999CC',
                 color: 'white'
             }}>This is BLOG page.</p>
-            <a href='/about'
-               style={{
-                    backgroundColor: 'black',
-                    color: 'white',
-                    padding: '3px 18px',
-                    boxSizing: 'border-box',
-                    border: '1px black solid',
-                    textDecoration: 'none',
-                    borderRadius: '9999px'
-               }}>ABOUTへジャンプ</a>
+            <Link href='/about'>
+                ABOUTへ
+            </Link>
         </div>
     )
 }
